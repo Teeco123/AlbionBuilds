8e57bffbec4b710c84a3d724d76c5c0b2d139e5b
Kacper <kacper.gajko1@gmail.com>
2023-05-22 01:53:57 +0200

    working select + displaying images
    All capes added
    Can't select offhand while weapons is 2handed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,97 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { weapons, offhands, capes, helmets, armors, boots } from "./components/items";
+
+	let selectedWeapon: { name: string; img: string } = { name: "", img: "" };
+	let selectedOffhand: { name: string; img: string } = { name: "", img: "" };
+	let selectedHelmet: { name: string; img: string } = { name: "", img: "" };
+	let selectedArmor: { name: string; img: string } = { name: "", img: "" };
+	let selectedBoots: { name: string; img: string } = { name: "", img: "" };
+	let selectedCape: { name: string; img: string } = { name: "", img: "" };
+</script>
+
+<body>
+	<div class="form">
+		<form>
+			<!-----------Weapon select------------->
+			<label>
+				Weapon:
+				<select bind:value={selectedWeapon}>
+					{#each weapons as weapon}
+						<option value={weapon}>
+							{weapon.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<!-----------Offhand select------------>
+			<label>
+				Offhand:
+				<select disabled={!selectedWeapon.onehand} bind:value={selectedOffhand}>
+					{#each offhands as offhand}
+						<option value={offhand}>
+							{offhand.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<!-----------Helmet select------------->
+			<label>
+				Helmet:
+				<select bind:value={selectedHelmet}>
+					{#each helmets as helmet}
+						<option value={helmet}>
+							{helmet.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<!-----------Armor select-------------->
+			<label>
+				Armor:
+				<select bind:value={selectedArmor}>
+					{#each armors as armor}
+						<option value={armor}>
+							{armor.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<!-----------Helmet select------------->
+			<label>
+				Boots:
+				<select bind:value={selectedBoots}>
+					{#each boots as boots}
+						<option value={boots}>
+							{boots.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<!------------Cape select-------------->
+			<label>
+				Cape:
+				<select bind:value={selectedCape}>
+					{#each capes as cape}
+						<option value={cape}>
+							{cape.name}
+						</option>
+					{/each}
+				</select>
+			</label>
+		</form>
+	</div>
+	{#if selectedWeapon.onehand == true}
+		<img src={selectedWeapon.img} alt={selectedWeapon.name} />
+		<img src={selectedOffhand.img} alt={selectedOffhand.name} />
+		<img src={selectedHelmet.img} alt={selectedHelmet.name} />
+		<img src={selectedArmor.img} alt={selectedArmor.name} />
+		<img src={selectedBoots.img} alt={selectedBoots.name} />
+		<img src={selectedCape.img} alt={selectedCape.name} />
+	{:else}
+		<img src={selectedWeapon.img} alt={selectedWeapon.name} />
+		<img src={selectedHelmet.img} alt={selectedHelmet.name} />
+		<img src={selectedArmor.img} alt={selectedArmor.name} />
+		<img src={selectedBoots.img} alt={selectedBoots.name} />
+		<img src={selectedCape.img} alt={selectedCape.name} />
+	{/if}
+</body>
---
+++ src/routes/components/items.ts
@@ -0,0 +1,101 @@
+export let weapons = [
+	{
+		name: "Nature Staff",
+		onehand: true,
+		img: "https://render.albiononline.com/v1/item/T8_MAIN_NATURESTAFF.png"
+	},
+	{
+		name: "Druidic Staff",
+		onehand: true,
+		img: "https://render.albiononline.com/v1/item/T8_MAIN_NATURESTAFF_KEEPER.png?"
+	},
+	{
+		name: "Great Nature",
+		onehand: false,
+		img: "https://render.albiononline.com/v1/item/T8_2H_NATURESTAFF.png?"
+	}
+];
+
+export let offhands = [
+	{
+		name: "Torch",
+		img: "https://render.albiononline.com/v1/item/T8_MAIN_NATURESTAFF.png"
+	},
+	{
+		name: "Mistcaller",
+		img: "https://render.albiononline.com/v1/item/T8_MAIN_NATURESTAFF_KEEPER.png?"
+	},
+	{
+		name: "Tome of spells",
+		img: "https://render.albiononline.com/v1/item/T8_2H_NATURESTAFF.png?"
+	}
+];
+
+export let helmets = [
+	{
+		name: "Graveguard helmet"
+	}
+];
+
+export let armors = [
+	{
+		name: "Robe of purity"
+	}
+];
+
+export let boots = [
+	{
+		name: "Royal shoes"
+	}
+];
+
+export let capes = [
+	{
+		name: "Bridgewatch",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Bridgewatch%20Cape.png?"
+	},
+	{
+		name: "Fort Sterling",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Fort%20Sterling%20Cape.png?locale=en"
+	},
+	{
+		name: "Lymhurst",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Lymhurst%20Cape.png?"
+	},
+	{
+		name: "Martlock",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Martlock%20Cape.png?"
+	},
+	{
+		name: "Thetford",
+		img: "https://render.albiononline.com/v1/item/T8_CAPEITEM_FW_THETFORD.png?"
+	},
+	{
+		name: "Caerleon",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Caerleon%20Cape.png?locale=en"
+	},
+	{
+		name: "Heretic",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Heretic%20Cape.png?locale=en"
+	},
+	{
+		name: "Keeper",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Keeper%20Cape.png?locale=en"
+	},
+	{
+		name: "Morgana",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Morgana%20Cape.png?locale=en"
+	},
+	{
+		name: "Undead",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Undead%20Cape.png?locale=en"
+	},
+	{
+		name: "Demon",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Demon%20Cape.png?locale=en"
+	},
+	{
+		name: "Avalonian",
+		img: "https://render.albiononline.com/v1/item/Elder's%20Avalonian%20Cape.png?locale=en"
+	}
+];
