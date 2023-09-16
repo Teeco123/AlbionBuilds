8cdf97c289510c1b45f6a5393852b4879de5789b
Kacper <kacper.gajko1@gmail.com>
2023-09-17 00:26:45 +0200

    added login page
    login page with login and register functionality + validation + cookie session

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,50 @@
+import { db, client } from "$lib/server/db.js";
+import { redirect } from "@sveltejs/kit";
+
+export const actions = {
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const login = data.get("login");
+		const password = data.get("password");
+
+		let user = await db.collection("Users").findOne({ login: { $eq: login }, password: { $eq: password } });
+
+		const userid = String(user?._id);
+
+		if (user) {
+			cookies.set("session", userid, {
+				path: "/",
+				httpOnly: true,
+				secure: true,
+				maxAge: 60 * 60 * 24 * 7
+			});
+			console.log(cookies.get("session"));
+			throw redirect(303, "/profile");
+		} else if (!login) {
+			return { loginRequired: true };
+		} else if (!password) {
+			return { passwordRequired: true };
+		} else if (!user) {
+			return { dontMatch: true };
+		}
+	},
+
+	register: async ({ request }) => {
+		const data = await request.formData();
+		const login = data.get("login");
+		const password = data.get("password");
+
+		let user = await db.collection("Users").findOne({ login: login });
+
+		if (!user && password && login) {
+			db.collection("Users").insertOne({ login: login, password: password });
+			return { success: true };
+		} else if (!login) {
+			return { loginRequired: true };
+		} else if (!password) {
+			return { passwordRequired: true };
+		} else if (user) {
+			return { userExists: true };
+		}
+	}
+};
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { ActionData } from "./$types";
+
+	export let form: ActionData;
+</script>
+
+<form method="post" action="?/login">
+	Login:
+	<input name="login" type="text" />
+	Password:
+	<input name="password" type="password" />
+	<button>Log in</button>
+	<button formaction="?/register">Register</button>
+
+	{#if form?.success}
+		<p>User registered succesfully!</p>
+	{:else if form?.userExists}
+		<p>User with this login already exists!</p>
+	{:else if form?.passwordRequired}
+		<p>Password is required!</p>
+	{:else if form?.loginRequired}
+		<p>Login is required!</p>
+	{:else if form?.dontMatch}
+		<p>Login or password is WRONG!</p>
+	{/if}
+</form>
